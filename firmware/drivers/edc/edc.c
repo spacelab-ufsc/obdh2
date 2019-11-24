6e8635887ad4554248fd8e90b1dcc256d264534a
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-11-24 12:04:50 -0300

    firmware: drivers: Initial structure of the EDC driver #23

---
+++ firmware/drivers/edc/edc.c
@@ -0,0 +1,53 @@
+/*
+ * edc.c
+ * 
+ * Copyright (C) 2019, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief EDC driver implementation.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 27/10/2019
+ * 
+ * \addtogroup edc
+ * \{
+ */
+
+#include "edc.h"
+
+int edc_init()
+{
+    return -1;
+}
+
+int edc_write_cmd(edc_cmd_t cmd)
+{
+    return -1;
+}
+
+int edc_read(uint8_t *data, uint16_t len)
+{
+    return -1;
+}
+
+//! \} End of edc group
