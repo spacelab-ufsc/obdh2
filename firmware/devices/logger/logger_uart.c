a6b1db6cc119bf043e30728b75d59059ff401bc0
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-12-03 19:50:16 -0300

    firmware: devices: Importing the log routines from the OBDH 1.0. closes #15

--- firmware/devices/logger/logger_uart.c
+++ firmware/devices/logger/logger_uart.c
@@ -1,43 +1,54 @@
 /*
- * devices.h
+ * logger_uart.c
  * 
  * Copyright (C) 2019, SpaceLab.
  * 
  * This file is part of OBDH 2.0.
  * 
  * OBDH 2.0 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * OBDH 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
 /**
- * \brief Devices layer.
+ * \brief Logger device UART implementation.
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
  * \version 0.1.0
  * 
- * \date 26/10/2019
+ * \date 03/11/2019
  * 
- * \defgroup devices Devices
+ * \defgroup logger_uart UART
+ * \ingroup logger
  * \{
  */
 
-#ifndef DEVICES_H_
-#define DEVICES_H_
+#include <hal/usci_a_uart.h>
 
-#include "logger/logger.h"
+#include "logger.h"
+#include "logger_config.h"
 
-#endif // DEVICES_H_
+bool logger_uart_init()
+{
+    uart0_setup(LOGGER_UART_BAUDRATE_BPS);
 
-//! \} End of devices group
+    return true;
+}
+
+void logger_uart_write_byte(uint8_t byte)
+{
+    uart_tx_char(byte);
+}
+
+//! \} End of logger_uart group
