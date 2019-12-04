fd05e44a2c2c3f8b8dbe85e71f0da1698f075a0d
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-12-04 20:57:36 -0300

    firmware: app: tasks: Adding the initial structure of the "Startup" task #33

---
+++ firmware/app/tasks/startup.c
@@ -0,0 +1,46 @@
+/*
+ * startup.c
+ * 
+ * Copyright (C) 2019, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Startup task implementation.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 04/12/2019
+ * 
+ * \addtogroup startup
+ * \{
+ */
+
+#include "startup.h"
+
+xTaskHandle xTaskStartupHandle;
+
+void vTaskStartup(void *pvParameters)
+{
+    // Delete the task
+    vTaskDelete(xTaskStartupHandle);
+}
+
+//! \} End of startup group
