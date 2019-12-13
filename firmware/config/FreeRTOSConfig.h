473b8b61d06b9ef74a863ecb3121c916287e98a2
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-12-12 23:14:39 -0300

    firmware: config: freertos: Disabling the tick hook flag and decreasing the heap size to 40 kB

--- firmware/config/FreeRTOSConfig.h
+++ firmware/config/FreeRTOSConfig.h
@@ -51,12 +51,12 @@
 
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
-#define configUSE_TICK_HOOK				1
+#define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( 16000000UL )
 #define configLFXT_CLOCK_HZ       		( 32768L )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 60 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 40 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 20 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
