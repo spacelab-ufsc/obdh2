cb73b854446a9c6cc752d8ff216ffa95f70c361d
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-01-12 13:37:38 -0300

    firmware: app: tasks: Adding the initial structure of the periodic system reset task #72

---
+++ firmware/app/tasks/system_reset.c
@@ -0,0 +1,53 @@
+/*
+ * system_reset.c
+ * 
+ * Copyright (C) 2019, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Periodic system reset task implementation.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 12/01/2020
+ * 
+ * \addtogroup system_reset
+ * \{
+ */
+
+#include "system_reset.h"
+
+xTaskHandle xTaskSystemResetHandle;
+
+void vTaskSystemReset(void *pvParameters)
+{
+    // Delay before the first cycle
+    vTaskDelay(pdMS_TO_TICKS(TASK_SYSTEM_RESET_INITIAL_DELAY_MS));
+
+    while(1)
+    {
+        TickType_t last_cycle = xTaskGetTickCount();
+
+        vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_SYSTEM_RESET_PERIOD_MS));
+    }
+}
+
+//! \} End of system_reset group
