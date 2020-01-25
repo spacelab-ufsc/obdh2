1a49a5ac693925d44ed8e0063121c7a06cce618f
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-01-25 14:19:55 -0300

    config: FreeRTOSConfig: Changing the CPU clock to 32 MHz #8 #84

--- firmware/config/FreeRTOSConfig.h
+++ firmware/config/FreeRTOSConfig.h
@@ -52,7 +52,7 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				0
-#define configCPU_CLOCK_HZ				( 16000000UL )
+#define configCPU_CLOCK_HZ				( 32000000UL )
 #define configLFXT_CLOCK_HZ       		( 32768L )
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
