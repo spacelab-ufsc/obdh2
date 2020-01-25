45bd7716290f7796728024960f0b59329ecaa648
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-01-25 14:15:28 -0300

    firmware: Adding the "system" directory and the MCU configuration routines #2 #84

---
+++ firmware/system/system.h
@@ -0,0 +1,43 @@
+/*
+ * system.h
+ * 
+ * Copyright (C) 2019, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief System layer definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.1.0
+ * 
+ * \date 25/01/2020
+ * 
+ * \defgroup system System
+ * \{
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+#include "clocks.h"
+
+#endif // SYSTEM_H_
+
+//! \} End of system group
