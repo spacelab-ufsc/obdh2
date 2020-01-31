fb8178bdbe7c4a3bbdcf901bdfc7a077605ca694
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-01-30 23:43:30 -0300

    firmware: drivers: i2c: Adding the implementation of the reception routine #42

--- firmware/drivers/i2c/i2c.c
+++ firmware/drivers/i2c/i2c.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.1.7
+ * \version 0.1.8
  * 
  * \date 07/12/2019
  * 
@@ -139,7 +139,61 @@ int i2c_write(i2c_port_t port, i2c_slave_adr_t adr, uint8_t *data, uint16_t len)
 
 int i2c_read(i2c_port_t port, i2c_slave_adr_t adr, uint8_t *data, uint16_t len)
 {
-    return -1;
+    // Verifies if the slave address is lesser than 7-bits
+    if (adr > 128)
+    {
+        return -1;  // Invalid slave address
+    }
+
+    uint16_t base_address;
+
+    switch(port)
+    {
+        case I2C_PORT_0:    base_address = USCI_B0_BASE;    break;
+        case I2C_PORT_1:    base_address = USCI_B1_BASE;    break;
+        case I2C_PORT_2:    base_address = USCI_B2_BASE;    break;
+        default:
+            return -1;  // Invalid I2C port
+    }
+
+    USCI_B_I2C_setSlaveAddress(base_address, adr);
+
+    USCI_B_I2C_setMode(base_address, USCI_B_I2C_RECEIVE_MODE);
+
+    USCI_B_I2C_enable(base_address);
+
+    if (len == 1)   // Single byte
+    {
+        // Initiate command to receive a single character from Slave
+        USCI_B_I2C_masterReceiveSingleStart(base_address);
+
+        // Grab data from data register
+        data[0] = USCI_B_I2C_masterReceiveSingle(base_address);
+    }
+    else            // Multiple bytes
+    {
+        // Wait for bus to be free
+        while(USCI_B_I2C_isBusBusy(base_address));
+
+        // Initialize multi reception
+        USCI_B_I2C_masterReceiveMultiByteStart(base_address);
+
+        uint16_t i = 0;
+        for(i=0; i<len-2; i++)
+        {
+            data[i] = USCI_B_I2C_masterReceiveMultiByteNext(base_address);
+        }
+
+        // Initiate end of reception -> Receive byte with NAK
+        data[i++] = USCI_B_I2C_masterReceiveMultiByteFinish(base_address);
+
+        // Receive last byte
+        data[i++] = USCI_B_I2C_masterReceiveMultiByteNext(base_address);
+    }
+
+    USCI_B_I2C_disable(base_address);
+
+    return 0;
 }
 
 //! \} End of i2c group
