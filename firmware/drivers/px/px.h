e11dacf4bb36b7eba55f511fe27a412abcee2bb1
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-03-31 22:44:56 -0300

    firmware: drivers: Initial structure of the Payload X driver #131

---
+++ firmware/drivers/px/px.h
@@ -0,0 +1,88 @@
+/*
+ * px.h
+ * 
+ * Copyright (C) 2020, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Payload X driver definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.3.5
+ * 
+ * \date 31/10/2020
+ * 
+ * \defgroup px Payload X
+ * \ingroup drivers
+ * \{
+ */
+
+#ifndef PX_H_
+#define PX_H_
+
+#include <stdint.h>
+
+#include <drivers/i2c/i2c.h>
+
+#define PX_SLAVE_ADDRESS            0x42    /**< 7-bit slave address. */
+
+/**
+ * \brief Payload X configuration parameters.
+ */
+typedef struct
+{
+    i2c_port_t port;
+    uint32_t bitrate;
+} px_config_t;
+
+/**
+ * \brief Device initialization.
+ *
+ * \param[in] config is the configuration parameters of the Payload X driver.
+ *
+ * \return The status/error code.
+ */
+int px_init(px_config_t config);
+
+/**
+ * \brief Writes data to the Payload X.
+ *
+ * \param[in] data is an array of bytes to write.
+ *
+ * \param[in] len is the number of bytes to write.
+ *
+ * \return The status/error code.
+ */
+int px_write(uint8_t *data, uint16_t len);
+
+/**
+ * \brief Reads data from the Payload X.
+ *
+ * \param[in,out] data is a pointer to store the read data.
+ *
+ * \param[in] len is the number of bytes to read.
+ *
+ * \return The status/error code.
+ */
+int px_read(uint8_t *data, uint16_t len);
+
+#endif /* PX_H_ */
+
+/** \} End of px group */
