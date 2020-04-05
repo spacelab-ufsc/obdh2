23ae310ab5bd54eb138896d52e16acd39c19bf80
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-04-05 00:07:04 -0300

    firmware: app: tasks: Adding the "CSP server" task to the tasks initialization routine #17 #137

--- firmware/config/config.h
+++ firmware/config/config.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.3.4
+ * \version 0.3.6
  * 
  * \date 26/10/2019
  * 
@@ -45,6 +45,7 @@
 #define CONFIG_TASK_READ_TEMP_ENABLED                   1
 #define CONFIG_TASK_BEACON_ENABLED                      1
 #define CONFIG_TASK_SAVE_TIME_ENABLED                   1
+#define CONFIG_TASK_CSP_SERVER_ENABLED                  0
 
 #define CONFIG_SATELLITE_CALLSIGN                       "PY0EFS"
 
