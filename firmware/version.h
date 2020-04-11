7ec8005a259c0c23aba67049077f3095740301de
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-04-11 19:17:28 -0300

    firmware: app: tasks: beacon: Waiting the startup task to finish before the first cycle #40

--- firmware/version.h
+++ firmware/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.3.9
+ * \version 0.3.10
  * 
  * \date 25/10/2019
  * 
@@ -36,7 +36,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define FIRMWARE_VERSION            "0.3.9"
+#define FIRMWARE_VERSION            "0.3.10"
 
 #define FIRMWARE_STATUS             "Development"
 
