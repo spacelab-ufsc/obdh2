d9a5418423856f2e154b383a8b7dbd9b7ff39e6f
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-04-19 17:40:42 -0300

    firmware: app: tasks: Adding the "Uplink" task to the tasks creation routine #17 #147

--- firmware/config/config.h
+++ firmware/config/config.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.3.9
+ * \version 0.3.14
  * 
  * \date 26/10/2019
  * 
@@ -44,6 +44,7 @@
 #define CONFIG_TASK_RADIO_RESET_ENABLED                 1
 #define CONFIG_TASK_READ_TEMP_ENABLED                   1
 #define CONFIG_TASK_BEACON_ENABLED                      1
+#define CONFIG_TASK_UPLINK_ENABLED                      1
 #define CONFIG_TASK_SAVE_TIME_ENABLED                   1
 #define CONFIG_TASK_CSP_SERVER_ENABLED                  0
 
