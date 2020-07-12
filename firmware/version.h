283a909441798fd858d74eb22e4d05f98da42101
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2020-07-12 16:22:51 -0300

    firmware: drivers: adc: Adding a flag to verify if the ADC driver is already initialized, and adding the daughterboard channels to the initialization routine #120

--- firmware/version.h
+++ firmware/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.4.0
+ * \version 0.4.1
  * 
  * \date 25/10/2019
  * 
@@ -36,7 +36,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define FIRMWARE_VERSION            "0.4.0"
+#define FIRMWARE_VERSION            "0.4.1"
 
 #define FIRMWARE_STATUS             "Development"
 
