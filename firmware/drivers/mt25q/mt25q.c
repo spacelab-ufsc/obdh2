d920982c4cae02e0657f3a9b5373b3d2aadf0f47
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-04-21 20:27:27 -0300

    firmware: Minimal functions of the MT25Q driver, README file fixes and v0.6 release #24

--- firmware/drivers/mt25q/mt25q.c
+++ firmware/drivers/mt25q/mt25q.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.5.31
+ * \version 0.6.0
  * 
  * \date 2019/11/15
  * 
@@ -35,6 +35,8 @@
 
 #include "mt25q.h"
 #include "mt25q_reg.h"
+
+#define MT25Q_DUMMY_BYTE        0x00
 
 int mt25q_init(void)
 {
@@ -55,8 +57,8 @@ int mt25q_init(void)
 
 int mt25q_read_device_id(mt25q_dev_id_t *dev_id)
 {
-    uint8_t cmd[4] = {MT25Q_READ_ID_REG, 0, 0, 0};
-    uint8_t ans[4] = {0};
+    uint8_t cmd[4] = {MT25Q_READ_ID_REG, MT25Q_DUMMY_BYTE, MT25Q_DUMMY_BYTE, MT25Q_DUMMY_BYTE};
+    uint8_t ans[4] = {MT25Q_DUMMY_BYTE};
 
     if (mt25q_spi_transfer(cmd, ans, 4) != 0)
     {
@@ -68,6 +70,139 @@ int mt25q_read_device_id(mt25q_dev_id_t *dev_id)
     dev_id->memory_capacity  = ans[3];
 
     return 0;
+}
+
+int mt25q_read_status(mt25q_status_t *status)
+{
+    uint8_t cmd[2] = {MT25Q_READ_STATUS_REGISTER, MT25Q_DUMMY_BYTE};
+    uint8_t ans[2] = {MT25Q_DUMMY_BYTE};
+
+    if (mt25q_spi_transfer(cmd, ans, 2) != 0)
+    {
+        return -1;
+    }
+
+    status->write_enable        = (ans[1] & MT25Q_REG_STATUS_WRITE_ENABLE) >> 7;
+    status->top_bottom          = (ans[1] & MT25Q_REG_STATUS_TOP_BOTTOM) >> 5;
+    status->bp                  = (ans[1] & MT25Q_REG_STATUS_BP) >> 2;
+    status->bp                  = (((status->bp >> 1) & 0x08) | status->bp) & 0x0F;
+    status->write_enable_latch  = (ans[1] & MT25Q_REG_STATUS_WRITE_ENABLE_LATCH) >> 1;
+    status->write_in_progress   = (ans[1] & MT25Q_REG_STATUS_WRITE_IN_PROGRESS);
+
+    return 0;
+}
+
+int mt25q_reset(void)
+{
+    uint8_t cmd = MT25Q_RESET_ENABLE;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    cmd = MT25Q_RESET_MEMORY;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+int mt25q_enter_deep_power_down(void)
+{
+    uint8_t cmd = MT25Q_ENTER_DEEP_POWER_DOWN;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+int mt25q_release_from_deep_power_down(void)
+{
+    uint8_t cmd = MT25Q_RELEASE_FROM_DEEP_POWER_DOWN;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+int mt25q_write_enable(void)
+{
+    uint8_t cmd = MT25Q_WRITE_ENABLE;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    uint8_t i = 0;
+    for(i=0; i<UINT8_MAX; i++)
+    {
+        mt25q_status_t status = {0};
+
+        mt25q_read_status(&status);
+
+        if (status.write_enable_latch == MT25Q_WRITE_ENABLED)
+        {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+int mt25q_write_disable(void)
+{
+    uint8_t cmd = MT25Q_WRITE_DISABLE;
+
+    if (mt25q_spi_write(&cmd, 1) != 0)
+    {
+        return -1;
+    }
+
+    uint8_t i = 0;
+    for(i=0; i<UINT8_MAX; i++)
+    {
+        mt25q_status_t status = {0};
+
+        mt25q_read_status(&status);
+
+        if (status.write_enable_latch == MT25Q_WRITE_DISABLED)
+        {
+            return 0;
+        }
+    }
+
+    return -1;
+}
+
+bool mt25q_is_busy(void)
+{
+    mt25q_status_t status = {0};
+
+    if (mt25q_read_status(&status) != 0)
+    {
+        /* Error reading the status register */
+        return true;
+    }
+
+    if (status.write_in_progress == MT25Q_READY)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
 int mt25q_write(uint32_t adr, uint8_t *data, uint32_t len)
--- firmware/drivers/mt25q/mt25q.h
+++ firmware/drivers/mt25q/mt25q.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.5.27
+ * \version 0.6.0
  * 
  * \date 2019/11/15
  * 
@@ -49,13 +49,20 @@
 #define MT25Q_MEMORY_TYPE_1V8           0xBB    /**< 1.8 V. */
 
 /* MT25Q memory capacity */
-#define MT25Q_MEMORY_CAPACITY_2GB       0x22    /**< 2 GB. */
-#define MT25Q_MEMORY_CAPACITY_1GB       0x21    /**< 1 GB. */
-#define MT25Q_MEMORY_CAPACITY_512MB     0x20    /**< 512 MB. */
-#define MT25Q_MEMORY_CAPACITY_256MB     0x19    /**< 256 MB. */
-#define MT25Q_MEMORY_CAPACITY_128MB     0x18    /**< 128 MB. */
-#define MT25Q_MEMORY_CAPACITY_64MB      0x17    /**< 64 MB. */
+#define MT25Q_MEMORY_CAPACITY_2GB       0x22    /**< 2 Gb. */
+#define MT25Q_MEMORY_CAPACITY_1GB       0x21    /**< 1 Gb. */
+#define MT25Q_MEMORY_CAPACITY_512MB     0x20    /**< 512 Mb. */
+#define MT25Q_MEMORY_CAPACITY_256MB     0x19    /**< 256 Mb. */
+#define MT25Q_MEMORY_CAPACITY_128MB     0x18    /**< 128 Mb. */
+#define MT25Q_MEMORY_CAPACITY_64MB      0x17    /**< 64 Mb. */
 
+/* Write in progress status */
+#define MT25Q_READY                     0       /**< The memory is ready to write data. */
+#define MT25Q_BUSY                      1       /**< The memoty is busy writing data. */
+
+/* Write protection */
+#define MT25Q_WRITE_DISABLED            0       /**< Write operations disabled. */
+#define MT25Q_WRITE_ENABLED             1       /**< Write operations enabled. */
 
 /**
  * \brief Device ID structure.
@@ -68,20 +75,122 @@ typedef struct
 } mt25q_dev_id_t;
 
 /**
+ * \brief Status structure.
+ */
+typedef struct
+{
+    uint8_t write_enable;                       /**< Write enable/disable of the status register. */
+    uint8_t top_bottom;                         /**< Determines whether the protected memory area defined by the block protect bits starts from the top or bottom of the memory array. */
+    uint8_t bp;                                 /**< Defines memory to be software protected against PROGRAM or ERASE operations. */
+    uint8_t write_enable_latch;                 /**< The device always powers up with this bit cleared to prevent inadvertent WRITE, PROGRAM, or ERASE operation. */
+    uint8_t write_in_progress;                  /**< Indicates a command cycle is in progress. */
+} mt25q_status_t;
+
+/**
  * \brief Driver initialization.
  *
- * \return The error/status code.
+ * \return The status/error code.
  */
 int mt25q_init(void);
+
+/**
+ * \brief Resets the device by software.
+ *
+ * To reset the device, the RESET ENABLE command must be followed by the RESET MEMORY
+ * command. When the two commands are executed, the device enters a power-on reset condition.
+ * It is recommended to exit XIP mode before executing these two commands.
+ *
+ * All volatile lock bits, the volatile configuration register, the enhanced volatile configuration
+ * register, and the extended address register are reset to the power-on reset default
+ * condition according to nonvolatile configuration register settings.
+ *
+ * If a reset is initiated while a WRITE, PROGRAM, or ERASE operation is in progress or suspended,
+ * the operation is aborted and data may be corrupted.
+ *
+ * Reset is effective after the flag status register bit 7 outputs 1 with at least one byte output.
+ *
+ * A RESET ENABLE command is not accepted during WRITE STATUS REGISTER and WRITE
+ * NONVOLATILE CONFIGURATION REGISTER operations.
+ *
+ * \return The status/error code.
+ */
+int mt25q_reset(void);
 
 /**
  * \brief Reads the device ID information.
  *
  * \param[in,out] dev_id is a pointer to store the read device ID.
  *
- * \return The error/status code.
+ * \return The status/error code.
  */
 int mt25q_read_device_id(mt25q_dev_id_t *dev_id);
+
+/**
+ * \brief Reads the status register.
+ *
+ * Can be read continuously and at any time, including during a PROGRAM, ERASE, or WRITE operation.
+ * If one of these operations is in progress, checking the write in progress bit or P/E controller bit is
+ * recommended before executing the command.
+ *
+ * \param[in,out] status is a pointer to the status structure to store the read data.
+ *
+ * \return The status/error code.
+ */
+int mt25q_read_status(mt25q_status_t *status);
+
+/**
+ * \brief Put the device in deep power-down.
+ *
+ * The command is used to place the device in deep power-down mode for the lowest device
+ * power consumption, with device current reduced to I_CC2. This command can also be used as
+ * a software protection mechanism while the device is not in active use.
+ *
+ * \return The status/error code.
+ */
+int mt25q_enter_deep_power_down(void);
+
+/**
+ * \brief Release the device from the deep power-down.
+ *
+ * The command is used to exit from deep power-down mode. The device also exits deep
+ * power-down mode upon:
+ * \parblock
+ *      - A power-down, entering standby mode with the next power-up.
+ *      - A hardware or software reset operation, entering standby mode with a recovery time as
+ *      specified in the AC Reset Specifications.
+ *      .
+ * \endparblock
+ *
+ * \return The status/error code.
+ */
+int mt25q_release_from_deep_power_down(void);
+
+/**
+ * \brief Sets the write enable latch bit before each PROGRAM, ERASE, and WRITE command.
+ *
+ * \return The status/error code.
+ */
+int mt25q_write_enable(void);
+
+/**
+ * \brief Clears the write enable latch bit.
+ *
+ * In case of a protection error, WRITE DISABLE will not clear the bit. Instead, a
+ * CLEAR FLAG STATUS REGISTER command must be issued to clear both flags.
+ *
+ * \return The status/error code.
+ */
+int mt25q_write_disable(void);
+
+/**
+ * \brief Checks if the memory is busy or not.
+ *
+ * This function checks the Write In Progress (WIP) bit to determine whether the Flash memory is busy
+ * with a Write, Program or Erase cycle.
+ *
+ * \return TRUE/FALSE if the memory is busy or not.
+ */
+bool mt25q_is_busy(void);
 
 /**
  * \brief Writes data to a given address.
@@ -92,7 +201,7 @@ int mt25q_read_device_id(mt25q_dev_id_t *dev_id);
  *
  * \param[in] len is the number of bytes to write.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_write(uint32_t adr, uint8_t *data, uint32_t len);
 
@@ -105,14 +214,14 @@ int mt25q_write(uint32_t adr, uint8_t *data, uint32_t len);
  *
  * \param[in] len is the number of bytes to write.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_read(uint32_t adr, uint8_t *data, uint32_t len);
 
 /**
  * \brief SPI interface initialization.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_spi_init(void);
 
@@ -123,7 +232,7 @@ int mt25q_spi_init(void);
  *
  * \param[in] len is the number of bytes to write.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_spi_write(uint8_t *data, uint16_t len);
 
@@ -134,7 +243,7 @@ int mt25q_spi_write(uint8_t *data, uint16_t len);
  *
  * \param[in] len is the number of bytes to write.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_spi_read(uint8_t *data, uint16_t len);
 
@@ -147,7 +256,7 @@ int mt25q_spi_read(uint8_t *data, uint16_t len);
  *
  * \param[in] len is the number of bytes of the transfer operation.
  *
- * \return Error/status code.
+ * \return The status/error code.
  */
 int mt25q_spi_transfer(uint8_t *wdata, uint8_t *rdata, uint16_t len);
 
@@ -156,7 +265,7 @@ int mt25q_spi_transfer(uint8_t *wdata, uint8_t *rdata, uint16_t len);
  *
  * This function initializes the pins "HOLD" and "RESET".
  *
- * \return The error/status code.
+ * \return The status/error code.
  */
 int mt25q_gpio_init(void);
 
@@ -165,7 +274,7 @@ int mt25q_gpio_init(void);
  *
  * \param[in] state is the state to set the hold pin (high/low or true/false).
  *
- * \return The error/status code.
+ * \return The status/error code.
  */
 int mt25q_gpio_set_hold(bool state);
 
@@ -174,7 +283,7 @@ int mt25q_gpio_set_hold(bool state);
  *
  * \param[in] state is the state to set the reset pin (high/low or true/false).
  *
- * \return The error/status code.
+ * \return The status/error code.
  */
 int mt25q_gpio_set_reset(bool state);
 
