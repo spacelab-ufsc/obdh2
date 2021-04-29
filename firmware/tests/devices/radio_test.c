7ad8fabeef5effe7ce6f0d3cbb5a7247c7e2f34e
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-04-28 22:20:43 -0300

    firmware: tests: Minimum working version of the unit test of the radio device #209 #214

--- firmware/tests/devices/radio_test.c
+++ firmware/tests/devices/radio_test.c
@@ -44,10 +44,18 @@
 #include <devices/radio/radio.h>
 #include <tests/mockups/si446x_wrap.h>
 
+#define RADIO_ID    0x4463
+
 static void radio_init_test(void **state)
 {
-    will_return(__wrap_si446x_spi_init, 0);
-    will_return(__wrap_si446x_reset, 0);
+    will_return(__wrap_si446x_spi_init,             SI446X_SUCCESS);
+    will_return(__wrap_si446x_reset,                SI446X_SUCCESS);
+    will_return(__wrap_si446x_part_info,            SI446X_SUCCESS);
+    will_return(__wrap_si446x_part_info,            RADIO_ID);
+    will_return(__wrap_si446x_configuration_init,   SI446X_SUCCESS);
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_fifo_info,            SI446X_SUCCESS);
 
     int result = radio_init();
 
@@ -56,6 +64,23 @@ static void radio_init_test(void **state)
 
 static void radio_send_test(void **state)
 {
+    uint32_t timeout_ms = 100;
+
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_fifo_info,            SI446X_SUCCESS);
+    will_return(__wrap_si446x_write_tx_fifo,        SI446X_SUCCESS);
+    will_return(__wrap_si446x_get_int_status,       SI446X_SUCCESS);
+    will_return(__wrap_si446x_start_tx,             SI446X_SUCCESS);
+
+    timeout_ms /= 10;
+
+    while(timeout_ms--)
+    {
+        will_return(__wrap_si446x_get_int_status, SI446X_SUCCESS);
+
+        expect_function_call(__wrap_si446x_delay_ms);
+    }
+
     uint8_t data[50] = {0};
 
     int result = radio_send(data, 50, 100);
@@ -65,6 +90,12 @@ static void radio_send_test(void **state)
 
 static void radio_recv_test(void **state)
 {
+    will_return(__wrap_si446x_change_state,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_set_property,         SI446X_SUCCESS);
+    will_return(__wrap_si446x_start_rx,             SI446X_SUCCESS);
+
     uint8_t data[50] = {0};
 
     int result = radio_recv(data, 10, 1000);
@@ -74,6 +105,9 @@ static void radio_recv_test(void **state)
 
 static void radio_available_test(void **state)
 {
+    will_return(__wrap_si446x_get_int_status,       SI446X_SUCCESS);
+    will_return(__wrap_si446x_fifo_info,            SI446X_SUCCESS);
+
     int result = radio_available();
 
     assert_return_code(result, 0);
@@ -81,6 +115,8 @@ static void radio_available_test(void **state)
 
 static void radio_sleep_test(void **state)
 {
+    will_return(__wrap_si446x_change_state, SI446X_SUCCESS);
+
     int result = radio_sleep();
 
     assert_return_code(result, 0);
