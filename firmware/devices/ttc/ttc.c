98bf971c824448f06213d42fb91a10baf17d75ef
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-05-14 21:36:06 -0300

    firmware: devices: ttc: Implementation of the device #111

--- firmware/devices/ttc/ttc.c
+++ firmware/devices/ttc/ttc.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.6.17
+ * \version 0.6.19
  * 
  * \date 2020/02/01
  * 
@@ -33,36 +33,137 @@
  * \{
  */
 
+#include <system/sys_log/sys_log.h>
+
 #include "ttc.h"
 
 int ttc_init(ttc_config_t config)
 {
-    return -1;
+    sys_log_print_event_from_module(SYS_LOG_INFO, TTC_MODULE_NAME, "Initializing TTC device ");
+    sys_log_print_uint(config.id);
+    sys_log_print_msg("...");
+    sys_log_new_line();
+
+    if (sl_ttc2_init(config) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error initializing the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    uint8_t hw_ver = 0;
+    if (sl_ttc2_read_hardware_version(config, &hw_ver) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error reading the hardware version of the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    uint32_t fw_ver = 0;
+    if (sl_ttc2_read_firmware_version(config, &fw_ver) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error reading the firmware version of the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    sys_log_print_event_from_module(SYS_LOG_INFO, TTC_MODULE_NAME, "SpaceLab TTC 2.0 detected! (hw=");
+    sys_log_print_uint(hw_ver);
+    sys_log_print_msg(", fw=");
+    sys_log_print_uint(fw_ver);
+    sys_log_print_msg(")");
+    sys_log_new_line();
+
+    return 0;
 }
 
 int ttc_get_data(ttc_config_t config, ttc_data_t *data)
 {
-    return -1;
+    if (sl_ttc2_check_device(config) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error reading the data from the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("! No device detected!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    if (sl_ttc2_read_hk_data(config, data) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error reading the data from the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    return 0;
 }
 
 int ttc_send(ttc_config_t config, uint8_t *data, uint16_t len)
 {
-    return -1;
+    if (sl_ttc2_check_device(config) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error sending data to the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("! No device detected!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    if (sl_ttc2_transmit_packet(config, data, len) != 0)
+    {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TTC_MODULE_NAME, "Error sending data to the TTC device ");
+        sys_log_print_uint(config.id);
+        sys_log_print_msg("!");
+        sys_log_new_line();
+
+        return -1;
+    }
+
+    return 0;
+}
+
+int ttc_recv(ttc_config_t config, uint8_t *data, uint16_t *len)
+{
+    if (ttc_avail(config) <= 0)
+    {
+        return -1;  /* No packet to receive! */
+    }
+
+    if (sl_ttc2_read_packet(config, data, len) != 0)
+    {
+        return -1;
+    }
+
+    return 0;
 }
 
 int ttc_avail(ttc_config_t config)
 {
-    return -1;
+    return sl_ttc2_check_pkt_avail(config);
 }
 
 int ttc_enter_hibernation(ttc_config_t config)
 {
-    return -1;
+    return sl_ttc2_set_tx_enable(config, false);
 }
 
 int ttc_leave_hibernation(ttc_config_t config)
 {
-    return -1;
+    return sl_ttc2_set_tx_enable(config, true);
 }
 
 /** \} End of ttc group */
