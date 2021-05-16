a1a54089874774cf5a8137772c7ee589e876db91
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-05-16 02:46:16 -0300

    firmware: app: libs: Updating the Reliance Edge library to v2.5.1 #150

---
+++ firmware/app/libs/reliance-edge-2.5.1/os/freertos/services/osbdev_ramdisk.h
@@ -0,0 +1,273 @@
+/*             ----> DO NOT REMOVE THE FOLLOWING NOTICE <----
+
+                  Copyright (c) 2014-2020 Tuxera US Inc.
+                      All Rights Reserved Worldwide.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; use version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but "AS-IS," WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+/*  Businesses and individuals that for commercial or other reasons cannot
+    comply with the terms of the GPLv2 license must obtain a commercial license
+    before incorporating Reliance Edge into proprietary software for
+    distribution in any form.  Visit http://www.datalight.com/reliance-edge for
+    more information.
+*/
+/** @brief
+    @file FreeRTOS block device implementation; see osbdev.c for details.
+*/
+#ifndef OSBDEV_RAMDISK_H
+#define OSBDEV_RAMDISK_H
+
+
+#include <stdlib.h> /* For ALLOCATE_CLEARED_MEMORY(), which expands to calloc(). */
+
+
+static uint8_t *gapbRamDisk[REDCONF_VOLUME_COUNT];
+
+
+/** @brief Initialize a disk.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    initialized.
+    @param mode     The open mode, indicating the type of access required.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL Invalid sector geometry for a RAM disk.
+    @retval -RED_EIO    A disk I/O error occurred.
+*/
+static REDSTATUS DiskOpen(
+    uint8_t         bVolNum,
+    BDEVOPENMODE    mode)
+{
+    REDSTATUS       ret = 0;
+    const VOLCONF  *pVolConf = gaRedVolConf[bVolNum];
+
+    (void)mode;
+
+    if(pVolConf->ullSectorOffset > 0U)
+    {
+        /*  A sector offset makes no sense for a RAM disk.  The feature exists
+            to enable partitioning, but we don't support having more than one
+            file system on a RAM disk.  Thus, having a sector offset would only
+            waste memory by making the RAM disk bigger.
+        */
+        REDERROR();
+        ret = -RED_EINVAL;
+    }
+    else if((pVolConf->ulSectorSize == SECTOR_SIZE_AUTO) || (pVolConf->ullSectorCount == SECTOR_COUNT_AUTO))
+    {
+        /*  Automatic geometry detection is not possible for RAM disks.
+        */
+        ret = -RED_EINVAL;
+    }
+    else if(gapbRamDisk[bVolNum] == NULL)
+    {
+        if((size_t)pVolConf->ullSectorCount != pVolConf->ullSectorCount)
+        {
+            REDERROR();
+            ret = -RED_EINVAL;
+        }
+        else
+        {
+            gapbRamDisk[bVolNum] = ALLOCATE_CLEARED_MEMORY(pVolConf->ullSectorCount, pVolConf->ulSectorSize);
+            if(gapbRamDisk[bVolNum] == NULL)
+            {
+                ret = -RED_EIO;
+            }
+        }
+    }
+    else
+    {
+        /*  RAM disk already exists, nothing to do.
+        */
+    }
+
+    return ret;
+}
+
+
+/** @brief Uninitialize a disk.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    uninitialized.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL RAM disk has not been created.
+*/
+static REDSTATUS DiskClose(
+    uint8_t     bVolNum)
+{
+    REDSTATUS   ret;
+
+    if(gapbRamDisk[bVolNum] == NULL)
+    {
+        REDERROR();
+        ret = -RED_EINVAL;
+    }
+    else
+    {
+        /*  This implementation uses dynamically allocated memory, but must
+            retain previously written data after the block device is closed, and
+            thus the memory cannot be freed and will remain allocated until
+            reboot.
+        */
+        ret = 0;
+    }
+
+    return ret;
+}
+
+
+/** @brief Return the disk geometry.
+
+    @param bVolNum  The volume number of the volume whose block device geometry
+                    is being queried.
+    @param pInfo    On successful return, populated with the geometry of the
+                    block device.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0               Operation was successful.
+    @retval -RED_EIO        A disk I/O or driver error occurred.
+    @retval -RED_ENOTSUPP   The geometry cannot be queried on this block device.
+*/
+static REDSTATUS DiskGetGeometry(
+    uint8_t     bVolNum,
+    BDEVINFO   *pInfo)
+{
+    (void)bVolNum;
+    (void)pInfo;
+
+    return -RED_ENOTSUPP;
+}
+
+
+/** @brief Read sectors from a disk.
+
+    @param bVolNum          The volume number of the volume whose block device
+                            is being read from.
+    @param ullSectorStart   The starting sector number.
+    @param ulSectorCount    The number of sectors to read.
+    @param pBuffer          The buffer into which to read the sector data.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL RAM disk has not been created.
+*/
+static REDSTATUS DiskRead(
+    uint8_t     bVolNum,
+    uint64_t    ullSectorStart,
+    uint32_t    ulSectorCount,
+    void       *pBuffer)
+{
+    REDSTATUS   ret;
+
+    if(gapbRamDisk[bVolNum] == NULL)
+    {
+        REDERROR();
+        ret = -RED_EINVAL;
+    }
+    else
+    {
+        uint64_t ullByteOffset = ullSectorStart * gaRedVolConf[bVolNum].ulSectorSize;
+        uint32_t ulByteCount = ulSectorCount * gaRedVolConf[bVolNum].ulSectorSize;
+
+        RedMemCpy(pBuffer, &gapbRamDisk[bVolNum][ullByteOffset], ulByteCount);
+
+        ret = 0;
+    }
+
+    return ret;
+}
+
+
+#if REDCONF_READ_ONLY == 0
+
+/** @brief Write sectors to a disk.
+
+    @param bVolNum          The volume number of the volume whose block device
+                            is being written to.
+    @param ullSectorStart   The starting sector number.
+    @param ulSectorCount    The number of sectors to write.
+    @param pBuffer          The buffer from which to write the sector data.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL RAM disk has not been created.
+*/
+static REDSTATUS DiskWrite(
+    uint8_t     bVolNum,
+    uint64_t    ullSectorStart,
+    uint32_t    ulSectorCount,
+    const void *pBuffer)
+{
+    REDSTATUS   ret;
+
+    if(gapbRamDisk[bVolNum] == NULL)
+    {
+        REDERROR();
+        ret = -RED_EINVAL;
+    }
+    else
+    {
+        uint64_t ullByteOffset = ullSectorStart * gaRedVolConf[bVolNum].ulSectorSize;
+        uint32_t ulByteCount = ulSectorCount * gaRedVolConf[bVolNum].ulSectorSize;
+
+        RedMemCpy(&gapbRamDisk[bVolNum][ullByteOffset], pBuffer, ulByteCount);
+
+        ret = 0;
+    }
+
+    return ret;
+}
+
+
+/** @brief Flush any caches beneath the file system.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    flushed.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL RAM disk has not been created.
+*/
+static REDSTATUS DiskFlush(
+    uint8_t     bVolNum)
+{
+    REDSTATUS   ret;
+
+    if(gapbRamDisk[bVolNum] == NULL)
+    {
+        REDERROR();
+        ret = -RED_EINVAL;
+    }
+    else
+    {
+        ret = 0;
+    }
+
+    return ret;
+}
+
+#endif /* REDCONF_READ_ONLY == 0 */
+
+
+#endif /* OSBDEV_RAMDISK_H */
+
---
+++ firmware/app/libs/reliance-edge-2.5.1/os/u-boot/services/osbdev.c
@@ -0,0 +1,283 @@
+/*             ----> DO NOT REMOVE THE FOLLOWING NOTICE <----
+
+                  Copyright (c) 2014-2020 Tuxera US Inc.
+                      All Rights Reserved Worldwide.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; use version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but "AS-IS," WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+/*  Businesses and individuals that for commercial or other reasons cannot
+    comply with the terms of the GPLv2 license must obtain a commercial license
+    before incorporating Reliance Edge into proprietary software for
+    distribution in any form.  Visit http://www.datalight.com/reliance-edge for
+    more information.
+*/
+/** @file
+    @brief Implements block device I/O.
+*/
+#include <common.h>
+#include <blk.h>
+#include <redfs_uboot.h>
+
+
+#include <redfs.h>
+#include <redvolume.h>
+#include <redbdev.h>
+#include <redosdeviations.h>
+#include <redutils.h>
+#include <redosbdev.h>
+
+
+/*  This array holds the block device handles and partition information for
+    the various redfs volumes.
+*/
+typedef struct {
+    struct blk_desc * block_dev;
+    disk_partition_t * fs_partition;
+} UBOOT_DEV;
+static UBOOT_DEV gaDisk[REDCONF_VOLUME_COUNT];
+
+
+/** @brief Configure a block device.
+
+    @note   This is a non-standard block device API!  The standard block device
+            APIs are designed for implementations running on targets with block
+            devices that are known in advance and can be statically defined by
+            the implementation.  However, that model does not work well for
+            u-boot, so we implement this API to allow the name of the block
+            device to be specified at run-time.
+
+    @param bVolNum      The volume number of the volume to configure.
+    @param pszBDevSpec  Drive or file to associate with the volume.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is not a valid volume number; or
+                        @p pszBDevSpec is `NULL` or an empty string.
+*/
+REDSTATUS RedOsBDevConfig2(
+    uint8_t bVolNum,
+    struct blk_desc * block_dev,
+    disk_partition_t * fs_partition)
+{
+    REDSTATUS ret = 0;
+
+
+    if(bVolNum >= REDCONF_VOLUME_COUNT)
+    {
+        ret = RED_EINVAL;
+    }
+    else
+    {
+        gaDisk[bVolNum].block_dev = block_dev;
+        gaDisk[bVolNum].fs_partition = fs_partition;
+    }
+
+
+    return ret;
+}
+
+
+/** @brief Initialize a block device.
+
+    This function is called when the file system needs access to a block
+    device.
+
+    Upon successful return, the block device should be fully initialized and
+    ready to service read/write/flush/close requests.
+
+    The behavior of calling this function on a block device which is already
+    open is undefined.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    initialized.
+    @param mode     The open mode, indicating the type of access required.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is an invalid volume number; or the block
+                        device is already open.
+    @retval -RED_EIO    A disk I/O error occurred.
+*/
+REDSTATUS RedOsBDevOpen(
+    uint8_t         bVolNum,
+    BDEVOPENMODE    mode)
+{
+    return 0;
+}
+
+
+/** @brief Uninitialize a block device.
+
+    This function is called when the file system no longer needs access to a
+    block device.  If any resource were allocated by RedOsBDevOpen() to service
+    block device requests, they should be freed at this time.
+
+    Upon successful return, the block device must be in such a state that it
+    can be opened again.
+
+    The behavior of calling this function on a block device which is already
+    closed is undefined.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    uninitialized.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is an invalid volume number; or the block
+                        device is already open.
+*/
+REDSTATUS RedOsBDevClose(
+    uint8_t     bVolNum)
+{
+    return 0;
+}
+
+
+/** @brief Return the block device geometry.
+
+    The behavior of calling this function is undefined if the block device is
+    closed.
+
+    @param bVolNum  The volume number of the volume whose block device geometry
+                    is being queried.
+    @param pInfo    On successful return, populated with the geometry of the
+                    block device.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval -RED_EINVAL     @p bVolNum is an invalid volume number, or @p pInfo
+                            is `NULL`.
+    @retval -RED_ENOTSUPP   The geometry cannot be queried on this block device.
+*/
+REDSTATUS RedOsBDevGetGeometry(
+    uint8_t     bVolNum,
+    BDEVINFO   *pInfo)
+{
+    REDSTATUS   ret;
+
+    if((bVolNum >= REDCONF_VOLUME_COUNT) || (pInfo == NULL))
+    {
+        ret = -RED_EINVAL;
+    }
+    else
+    {
+        ret = -RED_ENOTSUPP;
+    }
+
+    return ret;
+}
+
+
+/** @brief Read sectors from a physical block device.
+
+    The behavior of calling this function is undefined if the block device is
+    closed or if it was opened with ::BDEV_O_WRONLY.
+
+    @param bVolNum          The volume number of the volume whose block device
+                            is being read from.
+    @param ullSectorStart   The starting sector number.
+    @param ulSectorCount    The number of sectors to read.
+    @param pBuffer          The buffer into which to read the sector data.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is an invalid volume number, @p pBuffer is
+                        `NULL`, or @p ullStartSector and/or @p ulSectorCount
+                        refer to an invalid range of sectors.
+    @retval -RED_EIO    A disk I/O error occurred.
+*/
+REDSTATUS RedOsBDevRead(
+    uint8_t     bVolNum,
+    uint64_t    ullSectorStart,
+    uint32_t    ulSectorCount,
+    void       *pBuffer)
+{
+    ulong count;
+    REDSTATUS ret = 0;
+
+
+    count = blk_dread(gaDisk[bVolNum].block_dev, ullSectorStart, ulSectorCount, pBuffer);
+    if(count != ulSectorCount)
+    {
+        ret = -RED_EIO;
+    }
+
+
+    return ret;
+}
+
+
+#if REDCONF_READ_ONLY == 0
+/** @brief Write sectors to a physical block device.
+
+    The behavior of calling this function is undefined if the block device is
+    closed or if it was opened with ::BDEV_O_RDONLY.
+
+    @param bVolNum          The volume number of the volume whose block device
+                            is being written to.
+    @param ullSectorStart   The starting sector number.
+    @param ulSectorCount    The number of sectors to write.
+    @param pBuffer          The buffer from which to write the sector data.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is an invalid volume number, @p pBuffer is
+                        `NULL`, or @p ullStartSector and/or @p ulSectorCount
+                        refer to an invalid range of sectors.
+    @retval -RED_EIO    A disk I/O error occurred.
+*/
+REDSTATUS RedOsBDevWrite(
+    uint8_t     bVolNum,
+    uint64_t    ullSectorStart,
+    uint32_t    ulSectorCount,
+    const void *pBuffer)
+{
+    return -RED_EINVAL;
+}
+
+
+/** @brief Flush any caches beneath the file system.
+
+    This function must synchronously flush all software and hardware caches
+    beneath the file system, ensuring that all sectors written previously are
+    committed to permanent storage.
+
+    If the environment has no caching beneath the file system, the
+    implementation of this function can do nothing and return success.
+
+    The behavior of calling this function is undefined if the block device is
+    closed or if it was opened with ::BDEV_O_RDONLY.
+
+    @param bVolNum  The volume number of the volume whose block device is being
+                    flushed.
+
+    @return A negated ::REDSTATUS code indicating the operation result.
+
+    @retval 0           Operation was successful.
+    @retval -RED_EINVAL @p bVolNum is an invalid volume number.
+    @retval -RED_EIO    A disk I/O error occurred.
+*/
+REDSTATUS RedOsBDevFlush(
+    uint8_t     bVolNum)
+{
+    return -RED_EINVAL;
+}
+#endif /* REDCONF_READ_ONLY == 0 */
+
+
