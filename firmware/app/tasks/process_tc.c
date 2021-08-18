b49210c794160ae81bef387dece7f66f4dada36d
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-08-18 20:42:21 -0300

    firmware: app: tasks: process_tc: Initial implementation #275

--- firmware/app/tasks/process_tc.c
+++ firmware/app/tasks/process_tc.c
@@ -55,7 +55,51 @@ void vTaskProcessTC(void *pvParameters)
     {
         TickType_t last_cycle = xTaskGetTickCount();
 
-        /* TODO */
+        int pkts = ttc_avail(TTC_1);
+
+        if (pkts > 0)
+        {
+            sys_log_print_event_from_module(SYS_LOG_INFO, TASK_PROCESS_TC_NAME, "New ");
+            sys_log_print_uint((unsigned int)pkts);
+            sys_log_print_msg(" packet(s) available!");
+            sys_log_new_line();
+
+            uint8_t pkt[300] = {0xFF};
+            uint16_t pkt_len = 0;
+            if (ttc_recv(TTC_1, pkt, &pkt_len) != 0)
+            {
+                switch(pkt[0])
+                {
+                    case CONFIG_PKT_ID_UPLINK_PING_REQ:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_DATA_REQ:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_BROADCAST_MSG:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_ENTER_HIBERNATION:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_LEAVE_HIBERNATION:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_ACTIVATE_MODULE:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_DEACTIVATE_MODULE:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_ACTIVATE_PAYLOAD:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_DEACTIVATE_PAYLOAD:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_ERASE_MEMORY:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_FORCE_RESET:
+                        break;
+                    case CONFIG_PKT_ID_UPLINK_GET_PAYLOAD_DATA:
+                        break;
+                    default:
+                        sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_PROCESS_TC_NAME, "Unknow packet received!");
+                        sys_log_new_line();
+                }
+            }
+        }
 
         vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_PROCESS_TC_PERIOD_MS));
     }
