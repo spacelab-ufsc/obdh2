041c58a792e87ca3dc4525b5fb72632d1fef9a85
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-08-31 10:12:41 -0300

    firmware: tests: drivers: Implementation of the unit test of the CY15x102QN driver #276

--- firmware/tests/drivers/cy15x102qn_test.c
+++ firmware/tests/drivers/cy15x102qn_test.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.7.10
+ * \version 0.7.11
  * 
  * \date 2021/08/29
  * 
@@ -275,50 +275,282 @@ static void cy15x102qn_hibernate_mode_test(void **state)
 
 static void cy15x102qn_spi_init_test(void **state)
 {
+    expect_value(__wrap_spi_init, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_init, config.speed_hz, CY15X102QN_SPI_CLOCK_HZ);
+    expect_value(__wrap_spi_init, config.mode, CY15X102QN_SPI_MODE);
+
+    will_return(__wrap_spi_init, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_init(&conf), 0);
 }
 
 static void cy15x102qn_spi_write_test(void **state)
 {
+    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, CY15X102QN_SPI_CS);
+    expect_memory(__wrap_spi_write, data, (void*)data_test, len_test);
+    expect_value(__wrap_spi_write, len, len_test);
+
+    will_return(__wrap_spi_write, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_write(&conf, data_test, len_test), 0);
 }
 
 static void cy15x102qn_spi_read_test(void **state)
 {
+    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_read, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_read, cs, CY15X102QN_SPI_CS);
+    expect_value(__wrap_spi_read, len, len_test);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        will_return(__wrap_spi_read, data_test[i]);
+    }
+
+    will_return(__wrap_spi_read, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_read(&conf, data_test, len_test), 0);
 }
 
 static void cy15x102qn_spi_transfer_test(void **state)
 {
+    uint8_t wd_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint8_t rd_test[10] = {0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_transfer, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_transfer, cs, CY15X102QN_SPI_CS);
+    expect_memory(__wrap_spi_transfer, wd, (void*)wd_test, len_test);
+    expect_value(__wrap_spi_transfer, len, len_test);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        will_return(__wrap_spi_transfer, rd_test[i]);
+    }
+
+    will_return(__wrap_spi_transfer, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    uint8_t rd_res[10] = {0xFF};
+
+    assert_return_code(cy15x102qn_spi_transfer(&conf, wd_test, rd_res, len_test), 0);
+
+    for(i=0; i<len_test; i++)
+    {
+        assert_int_equal(rd_test[i], rd_res[i]);
+    }
 }
 
 static void cy15x102qn_spi_select_test(void **state)
 {
+    expect_value(__wrap_spi_select_slave, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_select_slave, cs, CY15X102QN_SPI_CS);
+    expect_value(__wrap_spi_select_slave, active, true);
+
+    will_return(__wrap_spi_select_slave, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_select(&conf), 0);
 }
 
 static void cy15x102qn_spi_unselect_test(void **state)
 {
+    expect_value(__wrap_spi_select_slave, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_select_slave, cs, CY15X102QN_SPI_CS);
+    expect_value(__wrap_spi_select_slave, active, false);
+
+    will_return(__wrap_spi_select_slave, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_unselect(&conf), 0);
 }
 
 static void cy15x102qn_spi_write_only_test(void **state)
 {
+    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, SPI_CS_NONE);
+    expect_memory(__wrap_spi_write, data, (void*)data_test, len_test);
+    expect_value(__wrap_spi_write, len, len_test);
+
+    will_return(__wrap_spi_write, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_write_only(&conf, data_test, len_test), 0);
 }
 
 static void cy15x102qn_spi_read_only_test(void **state)
 {
+    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_read, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_read, cs, SPI_CS_NONE);
+    expect_value(__wrap_spi_read, len, len_test);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        will_return(__wrap_spi_read, data_test[i]);
+    }
+
+    will_return(__wrap_spi_read, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_spi_read_only(&conf, data_test, len_test), 0);
 }
 
 static void cy15x102qn_spi_transfer_only_test(void **state)
 {
+    uint8_t wd_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
+    uint8_t rd_test[10] = {0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
+    uint16_t len_test = 10;
+
+    expect_value(__wrap_spi_transfer, port, CY15X102QN_SPI_PORT);
+    expect_value(__wrap_spi_transfer, cs, SPI_CS_NONE);
+    expect_memory(__wrap_spi_transfer, wd, (void*)wd_test, len_test);
+    expect_value(__wrap_spi_transfer, len, len_test);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        will_return(__wrap_spi_transfer, rd_test[i]);
+    }
+
+    will_return(__wrap_spi_transfer, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    uint8_t rd_res[10] = {0xFF};
+
+    assert_return_code(cy15x102qn_spi_transfer_only(&conf, wd_test, rd_res, len_test), 0);
+
+    for(i=0; i<len_test; i++)
+    {
+        assert_int_equal(rd_test[i], rd_res[i]);
+    }
 }
 
 static void cy15x102qn_gpio_init_test(void **state)
 {
+    expect_value(__wrap_gpio_init, pin, CY15X102QN_GPIO_WP_PIN);
+    expect_value(__wrap_gpio_init, config.mode, GPIO_MODE_OUTPUT);
+
+    will_return(__wrap_gpio_init, 0);
+
+    expect_value(__wrap_gpio_set_state, pin, CY15X102QN_GPIO_WP_PIN);
+    expect_value(__wrap_gpio_set_state, level, true);
+
+    will_return(__wrap_gpio_set_state, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_gpio_init(&conf), 0);
 }
 
 static void cy15x102qn_gpio_set_write_protect_test(void **state)
 {
+    expect_value(__wrap_gpio_set_state, pin, CY15X102QN_GPIO_WP_PIN);
+    expect_value(__wrap_gpio_set_state, level, true);
+
+    will_return(__wrap_gpio_set_state, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_gpio_set_write_protect(&conf), 0);
 }
 
 static void cy15x102qn_gpio_clear_write_protect_test(void **state)
 {
+    expect_value(__wrap_gpio_set_state, pin, CY15X102QN_GPIO_WP_PIN);
+    expect_value(__wrap_gpio_set_state, level, false);
+
+    will_return(__wrap_gpio_set_state, 0);
+
+    cy15x102qn_config_t conf = {0};
+
+    conf.port       = CY15X102QN_SPI_PORT;
+    conf.cs_pin     = CY15X102QN_SPI_CS;
+    conf.clock_hz   = CY15X102QN_SPI_CLOCK_HZ;
+    conf.wp_pin     = CY15X102QN_GPIO_WP_PIN;
+
+    assert_return_code(cy15x102qn_gpio_clear_write_protect(&conf), 0);
 }
 
 int main(void)
