d25f84098da11f712c17ccca4a817936fbef3c31
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-09-01 01:05:36 -0300

    firmware: tests: drivers: Implementation of the unit test of the CY15x102QN driver #276

--- firmware/tests/drivers/cy15x102qn_test.c
+++ firmware/tests/drivers/cy15x102qn_test.c
@@ -41,6 +41,8 @@
 #include <float.h>
 #include <cmocka.h>
 
+#include <stdlib.h>
+
 #include <config/config.h>
 #include <drivers/spi/spi.h>
 #include <drivers/gpio/gpio.h>
@@ -51,8 +53,11 @@
 #define CY15X102QN_SPI_CLOCK_HZ     1000000UL
 #define CY15X102QN_SPI_MODE         SPI_MODE_0
 #define CY15X102QN_GPIO_WP_PIN      GPIO_PIN_62
+#define CY15X102QN_MAX_ADDRESS      0x0003FFFF
 
 cy15x102qn_config_t conf = {0};
+
+unsigned int generate_random(unsigned int l, unsigned int r);
 
 static void cy15x102qn_init_test(void **state)
 {
@@ -162,22 +167,35 @@ static void cy15x102qn_write_test(void **state)
     will_return(__wrap_spi_write, 0);
 
     /* Write address */
-    uint8_t adr[3] = {0x01, 0x02, 0x03};
+    uint32_t adr = generate_random(0, CY15X102QN_MAX_ADDRESS);
+
+    uint8_t adr_arr[3] = {0x00};
+
+    adr_arr[0] = (uint8_t)(adr >> 16) & 0xFF;
+    adr_arr[1] = (uint8_t)(adr >> 8) & 0xFF;
+    adr_arr[2] = (uint8_t)(adr >> 0) & 0xFF;
 
     expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_write, cs, SPI_CS_NONE);
-    expect_memory(__wrap_spi_write, data, (void*)adr, 3);
+    expect_memory(__wrap_spi_write, data, (void*)adr_arr, 3);
     expect_value(__wrap_spi_write, len, 3);
 
     will_return(__wrap_spi_write, 0);
 
     /* Write data */
-    uint8_t data[5] = {0x21, 0x8F, 0x96, 0xFC, 0x32};
+    uint8_t data[256] = {0xFF};
+    uint16_t data_len = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<data_len; i++)
+    {
+        data[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_write, cs, SPI_CS_NONE);
-    expect_memory(__wrap_spi_write, data, (void*)data, 5);
-    expect_value(__wrap_spi_write, len, 5);
+    expect_memory(__wrap_spi_write, data, (void*)data, data_len);
+    expect_value(__wrap_spi_write, len, data_len);
 
     will_return(__wrap_spi_write, 0);
 
@@ -188,9 +206,7 @@ static void cy15x102qn_write_test(void **state)
 
     will_return(__wrap_spi_select_slave, 0);
 
-    uint32_t adr32 = ((uint32_t)adr[0] << 16) | ((uint32_t)adr[1] << 8) | adr[2];
-
-    assert_return_code(cy15x102qn_write(&conf, adr32, data, 5), 0);
+    assert_return_code(cy15x102qn_write(&conf, adr, data, data_len), 0);
 }
 
 static void cy15x102qn_read_test(void **state)
@@ -246,8 +262,14 @@ static void cy15x102qn_spi_init_test(void **state)
 
 static void cy15x102qn_spi_write_test(void **state)
 {
-    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint16_t len_test = 10;
+    uint8_t data_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        data_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_write, cs, CY15X102QN_SPI_CS);
@@ -261,14 +283,19 @@ static void cy15x102qn_spi_write_test(void **state)
 
 static void cy15x102qn_spi_read_test(void **state)
 {
-    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint16_t len_test = 10;
+    uint8_t data_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        data_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_read, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_read, cs, CY15X102QN_SPI_CS);
     expect_value(__wrap_spi_read, len, len_test);
 
-    uint16_t i = 0;
     for(i=0; i<len_test; i++)
     {
         will_return(__wrap_spi_read, data_test[i]);
@@ -281,16 +308,22 @@ static void cy15x102qn_spi_read_test(void **state)
 
 static void cy15x102qn_spi_transfer_test(void **state)
 {
-    uint8_t wd_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint8_t rd_test[10] = {0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
-    uint16_t len_test = 10;
+    uint8_t wd_test[256] = {0xFF};
+    uint8_t rd_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        wd_test[i] = generate_random(0, 255);
+        rd_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_transfer, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_transfer, cs, CY15X102QN_SPI_CS);
     expect_memory(__wrap_spi_transfer, wd, (void*)wd_test, len_test);
     expect_value(__wrap_spi_transfer, len, len_test);
 
-    uint16_t i = 0;
     for(i=0; i<len_test; i++)
     {
         will_return(__wrap_spi_transfer, rd_test[i]);
@@ -298,7 +331,7 @@ static void cy15x102qn_spi_transfer_test(void **state)
 
     will_return(__wrap_spi_transfer, 0);
 
-    uint8_t rd_res[10] = {0xFF};
+    uint8_t rd_res[256] = {0xFF};
 
     assert_return_code(cy15x102qn_spi_transfer(&conf, wd_test, rd_res, len_test), 0);
 
@@ -332,8 +365,14 @@ static void cy15x102qn_spi_unselect_test(void **state)
 
 static void cy15x102qn_spi_write_only_test(void **state)
 {
-    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint16_t len_test = 10;
+    uint8_t data_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        data_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_write, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_write, cs, SPI_CS_NONE);
@@ -347,14 +386,19 @@ static void cy15x102qn_spi_write_only_test(void **state)
 
 static void cy15x102qn_spi_read_only_test(void **state)
 {
-    uint8_t data_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint16_t len_test = 10;
+    uint8_t data_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        data_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_read, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_read, cs, SPI_CS_NONE);
     expect_value(__wrap_spi_read, len, len_test);
 
-    uint16_t i = 0;
     for(i=0; i<len_test; i++)
     {
         will_return(__wrap_spi_read, data_test[i]);
@@ -367,16 +411,22 @@ static void cy15x102qn_spi_read_only_test(void **state)
 
 static void cy15x102qn_spi_transfer_only_test(void **state)
 {
-    uint8_t wd_test[10] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-    uint8_t rd_test[10] = {0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13};
-    uint16_t len_test = 10;
+    uint8_t wd_test[256] = {0xFF};
+    uint8_t rd_test[256] = {0xFF};
+    uint16_t len_test = generate_random(1, 256);
+
+    uint16_t i = 0;
+    for(i=0; i<len_test; i++)
+    {
+        wd_test[i] = generate_random(0, 255);
+        rd_test[i] = generate_random(0, 255);
+    }
 
     expect_value(__wrap_spi_transfer, port, CY15X102QN_SPI_PORT);
     expect_value(__wrap_spi_transfer, cs, SPI_CS_NONE);
     expect_memory(__wrap_spi_transfer, wd, (void*)wd_test, len_test);
     expect_value(__wrap_spi_transfer, len, len_test);
 
-    uint16_t i = 0;
     for(i=0; i<len_test; i++)
     {
         will_return(__wrap_spi_transfer, rd_test[i]);
@@ -384,7 +434,7 @@ static void cy15x102qn_spi_transfer_only_test(void **state)
 
     will_return(__wrap_spi_transfer, 0);
 
-    uint8_t rd_res[10] = {0xFF};
+    uint8_t rd_res[256] = {0xFF};
 
     assert_return_code(cy15x102qn_spi_transfer_only(&conf, wd_test, rd_res, len_test), 0);
 
@@ -468,6 +518,11 @@ int main(void)
     };
 
     return cmocka_run_group_tests(cy15x102qn_tests, NULL, NULL);
+}
+
+unsigned int generate_random(unsigned int l, unsigned int r)
+{
+    return (rand() % (r - l + 1)) + l;
 }
 
 /** \} End of cy15x102qn_test group */
