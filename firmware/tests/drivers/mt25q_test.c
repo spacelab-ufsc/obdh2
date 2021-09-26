4cb395ddf83c005517ae3b75c31b85787065c6a1
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-09-25 21:17:02 -0300

    firmware: tests: drivers: Adding the implementation of the unit test of the MT25Q driver #286

--- firmware/tests/drivers/mt25q_test.c
+++ firmware/tests/drivers/mt25q_test.c
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.7.12
+ * \version 0.7.20
  * 
  * \date 2021/09/05
  * 
@@ -47,14 +47,118 @@
 #include <drivers/spi/spi.h>
 #include <drivers/mt25q/mt25q.h>
 
+#define MT25Q_SPI_PORT              SPI_PORT_0
+#define MT25Q_SPI_CS_PIN            SPI_CS_2
+#define MT25Q_SPI_CLOCK_HZ          1000000UL
+#define MT25Q_SPI_MODE              SPI_MODE_0
+#define MT25Q_GPIO_HOLD_PIN         GPIO_PIN_26
+#define MT25Q_GPIO_RESET_PIN        GPIO_PIN_27
+
 unsigned int generate_random(unsigned int l, unsigned int r);
 
 static void mt25q_init_test(void **state)
 {
+    /* SPI init */
+//    expect_value(__wrap_spi_init, port, MT25Q_SPI_PORT);
+//    expect_value(__wrap_spi_init, config.speed_hz, MT25Q_SPI_CLOCK_HZ);
+//    expect_value(__wrap_spi_init, config.mode, MT25Q_SPI_MODE);
+//
+//    will_return(__wrap_spi_init, 0);
+
+    /* GPIO init */
+//    expect_value(__wrap_gpio_init, pin, MT25Q_GPIO_HOLD_PIN);
+//    expect_value(__wrap_gpio_init, config.mode, GPIO_MODE_OUTPUT);
+//
+//    will_return(__wrap_gpio_init, 0);
+//
+//    expect_value(__wrap_gpio_init, pin, MT25Q_GPIO_RESET_PIN);
+//    expect_value(__wrap_gpio_init, config.mode, GPIO_MODE_OUTPUT);
+//
+//    will_return(__wrap_gpio_init, 0);
+
+    /* Read device ID */
+//    uint8_t cmd[350] = {0x00};
+//    uint8_t ans[350] = {0x00};
+//
+//    cmd[0] = 0x9E;
+//
+//    ans[1] = 0x20;
+//    ans[2] = 0xBA;
+//    ans[3] = 0x21;
+//
+//    expect_value(__wrap_spi_transfer, port, MT25Q_SPI_PORT);
+//    expect_value(__wrap_spi_transfer, cs, MT25Q_SPI_CS_PIN);
+//    expect_memory(__wrap_spi_transfer, wd, (void*)cmd, 4);
+//    expect_value(__wrap_spi_transfer, len, 4);
+//
+//    uint16_t i = 0;
+//    for(i=0; i<4; i++)
+//    {
+//        will_return(__wrap_spi_transfer, ans[i]);
+//    }
+//
+//    will_return(__wrap_spi_transfer, 0);
+
+    /* Read the discovery parameter signature */
+//    cmd[0] = 0x5A;
+//
+//    ans[1] = 0x00;
+//    ans[2] = 0x00;
+//    ans[3] = 0x00;
+//    ans[4] = 0x00;
+//    ans[5] = 'S';
+//    ans[6] = 'F';
+//    ans[7] = 'D';
+//    ans[8] = 'P';
+//    ans[] = ;
+
+//    expect_value(__wrap_spi_transfer, port, MT25Q_SPI_PORT);
+//    expect_value(__wrap_spi_transfer, cs, MT25Q_SPI_CS_PIN);
+//    expect_memory(__wrap_spi_transfer, wd, (void*)cmd, 350);
+//    expect_value(__wrap_spi_transfer, len, 350);
+//
+//    i = 0;
+//    for(i=0; i<350; i++)
+//    {
+//        will_return(__wrap_spi_transfer, ans[i]);
+//    }
+//
+//    will_return(__wrap_spi_transfer, 0);
+//
+//    assert_return_code(mt25q_init(), 0);
 }
 
 static void mt25q_read_device_id_test(void **state)
 {
+    uint8_t cmd[256] = {0x00};
+    uint8_t ans[256] = {0x00};
+
+    cmd[0] = 0x9E;
+
+    ans[1] = 0x20;
+    ans[2] = 0xBA;
+    ans[3] = 0x21;
+
+    expect_value(__wrap_spi_transfer, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_transfer, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_transfer, wd, (void*)cmd, 4);
+    expect_value(__wrap_spi_transfer, len, 4);
+
+    uint16_t i = 0;
+    for(i=0; i<4; i++)
+    {
+        will_return(__wrap_spi_transfer, ans[i]);
+    }
+
+    will_return(__wrap_spi_transfer, 0);
+
+    mt25q_dev_id_t dev_id = {0};
+
+    assert_return_code(mt25q_read_device_id(&dev_id), 0);
+
+    assert_int_equal(ans[1], dev_id.manufacturer_id);
+    assert_int_equal(ans[2], dev_id.memory_type);
+    assert_int_equal(ans[3], dev_id.memory_capacity);
 }
 
 static void mt25q_read_flash_description_test(void **state)
@@ -67,14 +171,53 @@ static void mt25q_read_status_test(void **state)
 
 static void mt25q_reset_test(void **state)
 {
+    uint8_t cmd = 0x66;
+
+    expect_value(__wrap_spi_write, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_write, data, (void*)&cmd, 1);
+    expect_value(__wrap_spi_write, len, 1);
+
+    will_return(__wrap_spi_write, 0);
+
+    cmd = 0x99;
+
+    expect_value(__wrap_spi_write, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_write, data, (void*)&cmd, 1);
+    expect_value(__wrap_spi_write, len, 1);
+
+    will_return(__wrap_spi_write, 0);
+
+    assert_return_code(mt25q_reset(), 0);
 }
 
 static void mt25q_enter_deep_power_down_test(void **state)
 {
+    uint8_t cmd = 0xB9;
+
+    expect_value(__wrap_spi_write, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_write, data, (void*)&cmd, 1);
+    expect_value(__wrap_spi_write, len, 1);
+
+    will_return(__wrap_spi_write, 0);
+
+    assert_return_code(mt25q_enter_deep_power_down(), 0);
 }
 
 static void mt25q_release_from_deep_power_down_test(void **state)
 {
+    uint8_t cmd = 0xAB;
+
+    expect_value(__wrap_spi_write, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_write, data, (void*)&cmd, 1);
+    expect_value(__wrap_spi_write, len, 1);
+
+    will_return(__wrap_spi_write, 0);
+
+    assert_return_code(mt25q_release_from_deep_power_down(), 0);
 }
 
 static void mt25q_write_enable_test(void **state)
@@ -119,10 +262,40 @@ static void mt25q_enter_4_byte_address_mode_test(void **state)
 
 static void mt25q_read_flag_status_register_test(void **state)
 {
+    uint8_t cmd[5] = {0x70, 0x00};
+    uint8_t ans[5] = {0x00};
+
+    ans[1] = generate_random(0, UINT8_MAX);
+
+    expect_value(__wrap_spi_transfer, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_transfer, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_transfer, wd, (void*)cmd, 2);
+    expect_value(__wrap_spi_transfer, len, 2);
+
+    will_return(__wrap_spi_transfer, ans[0]);
+    will_return(__wrap_spi_transfer, ans[1]);
+
+    will_return(__wrap_spi_transfer, 0);
+
+    uint8_t flag = UINT8_MAX;
+
+    assert_return_code(mt25q_read_flag_status_register(&flag), 0);
+
+    assert_int_equal(ans[1], flag);
 }
 
 static void mt25q_clear_flag_status_register_test(void **state)
 {
+    uint8_t cmd = 0x50;
+
+    expect_value(__wrap_spi_write, port, MT25Q_SPI_PORT);
+    expect_value(__wrap_spi_write, cs, MT25Q_SPI_CS_PIN);
+    expect_memory(__wrap_spi_write, data, (void*)&cmd, 1);
+    expect_value(__wrap_spi_write, len, 1);
+
+    will_return(__wrap_spi_write, 0);
+
+    assert_return_code(mt25q_clear_flag_status_register(), 0);
 }
 
 static void mt25q_get_flash_description_test(void **state)
