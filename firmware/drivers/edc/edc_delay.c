8376d7056303815b4a41854a5cd9f080264e85e9
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-10-03 03:36:04 -0300

    firmware: drivers: edc: Making the source files compliant with the MISRA-C:2012 standard

--- firmware/drivers/edc/edc_delay.c
+++ firmware/drivers/edc/edc_delay.c
@@ -1,7 +1,7 @@
 /*
  * edc_delay.c
  * 
- * Copyright (C) 2020, SpaceLab.
+ * Copyright (C) 2021, SpaceLab.
  * 
  * This file is part of OBDH 2.0.
  * 
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * along with OBDH 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
@@ -25,9 +25,9 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.2.21
+ * \version 0.7.38
  * 
- * \date 05/03/2020
+ * \date 2020/03/05
  * 
  * \defgroup edc_delay Delay
  * \ingroup edc
