f8459c44b0813301f4a97e88a97d37adc8cce19a
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2021-10-20 21:34:10 -0300

    firmware: config: Defining the preliminary keys of the private telecommands #298

---
+++ firmware/config/keys.h
@@ -0,0 +1,52 @@
+/*
+ * keys.h
+ * 
+ * Copyright (C) 2021, SpaceLab.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+/**
+ * \brief Keys definition.
+ * 
+ * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
+ * 
+ * \version 0.8.19
+ * 
+ * \date 2021/10/20
+ * 
+ * \defgroup keys Keys
+ * \{
+ */
+
+#ifndef KEYS_H_
+#define KEYS_H_
+
+/* Telecommands keys */
+#define CONFIG_TC_KEY_ENTER_HIBERNATION                 "#7@rsQVL$Pxf(5eU"
+#define CONFIG_TC_KEY_LEAVE_HIBERNATION                 "5d#QvdYzK59`Du>z"
+#define CONFIG_TC_KEY_ACTIVATE_MODULE                   "6mP=VA33u/:m)b+z"
+#define CONFIG_TC_KEY_DEACTIVATE_MODULE                 ":jvDVD_)jR@HWT{_"
+#define CONFIG_TC_KEY_ACTIVATE_PAYLOAD                  "`Uyp)]^n,3q[/zs["
+#define CONFIG_TC_KEY_DEACTIVATE_PAYLOAD                "GZ>'`Bs=dVGa[R#/"
+#define CONFIG_TC_KEY_ERASE_MEMORY                      "C:8RkJE${^>%wYVR"
+#define CONFIG_TC_KEY_FORCE_RESET                       "@A)Z-t(x+@nDHpKF"
+#define CONFIG_TC_KEY_GET_PAYLOAD_DATA                  "BkN&a):^fr(@(5x?"
+
+#endif /* KEYS_H_ */
+
+/** \} End of keys group */
