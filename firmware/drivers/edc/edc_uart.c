d0ff61d0e88a169c1cf71abb9f9171a384da1392
brunobene1 <brunobenedetti45@gmail.com>
2022-03-15 10:54:30 -0300

    firmware: drivers: edc: edc_uart.c created #310

---
+++ firmware/drivers/edc/edc_uart.c
@@ -0,0 +1,64 @@
+/*
+ * edc_uart.c
+ *
+ * Copyright (C) 2022, SpaceLab.
+ *
+ * This file is part of OBDH 2.0.
+ *
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \brief EDC driver UART routines implementation.
+ *
+ * \author Bruno Benedetti <brunobenedetti45gmail.com>
+ *
+ * \version 0.9.2
+ *
+ * \date 2022/03/15
+ *
+ * \addtogroup edc
+ * \{
+ */
+
+#include <drivers/uart/uart.h>
+
+#include "edc.h"
+
+int edc_uart_init()
+{
+    /* int uart_init(uart_port_t port, uart_config_t config); */
+    return -1;
+}
+
+int edc_uart_write()
+{
+    /* return uart_write(uart_port_t port, uint8_t *data, uint16_t len); */
+    return -1;
+}
+
+int edc_uart_read()
+{
+    /* return uart_read(uart_port_t port, uint8_t *data, uint16_t len); */
+    return -1;
+}
+
+int edc_uart_rx_available()
+{
+    /* return uart_available(uart_port_t port); */
+    return -1;
+}
+
+/** \} End of edc group */
