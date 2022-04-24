7b075a5a161138fd9a74218378b9af35108411a0
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2022-04-24 18:55:04 -0300

    firmware: libs: containers: Adapting the imported library #310

--- firmware/libs/containers/queue.c
+++ firmware/libs/containers/queue.c
@@ -1,22 +1,22 @@
 /*
  * queue.c
  * 
- * Copyright The TTC 2.0 Contributors.
+ * Copyright The OBDH 2.0 Contributors.
  * 
- * This file is part of TTC 2.0.
+ * This file is part of OBDH 2.0.
  * 
- * TTC 2.0 is free software: you can redistribute it and/or modify
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * TTC 2.0 is distributed in the hope that it will be useful,
+ * OBDH 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with TTC 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
+ * along with OBDH 2.0. If not, see <http:/\/www.gnu.org/licenses/>.
  * 
  */
 
@@ -26,7 +26,7 @@
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * \author Miguel Boing <miguelboing13@gmail.com>
  *
- * \version 0.1.16
+ * \version 0.9.2
  * 
  * \date 2017/11/08
  * 
