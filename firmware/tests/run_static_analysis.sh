28ec41626ad231daaca9fbc900907c01c06d2480
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2024-02-24 21:25:05 -0300

    firmware: tests: Adding the folders "structs" and "utils" to the static analysis test

--- firmware/tests/run_static_analysis.sh
+++ firmware/tests/run_static_analysis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cppcheck --error-exitcode=-1 --inline-suppr --addon=misra.json ../system
-cppcheck --error-exitcode=-1 --addon=misra.json ../devices
-cppcheck --std=c99 --error-exitcode=-1 --inline-suppr --addon=misra.json ../drivers
+cppcheck --force --error-exitcode=-1 --inline-suppr --addon=misra.json ../system
+cppcheck --force --error-exitcode=-1 --addon=misra.json ../devices
+cppcheck --std=c99 --force --error-exitcode=-1 --inline-suppr --addon=misra.json ../drivers
 cppcheck --std=c99 --force --error-exitcode=-1 --addon=misra.json ../app/tasks
+cppcheck --std=c99 --force --error-exitcode=-1 --addon=misra.json ../app/utils
+cppcheck --std=c99 --force --error-exitcode=-1 --addon=misra.json ../app/structs
