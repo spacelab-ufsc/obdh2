b804d5046c5572ad0518d0ef1b08591a565fd429
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2024-02-24 22:04:04 -0300

    firmware: Fixing MISRA-C violations and updating the static analysis workflow

--- firmware/app/utils/mem_mng.c
+++ firmware/app/utils/mem_mng.c
@@ -109,8 +109,9 @@ void mem_mng_load_obdh_data_from_default_values(obdh_telemetry_t *tel)
     uint8_t tle_line1[70] = OBDH_PARAM_POSITION_TLE_LINE1_DEFAULT_VAL;
     uint8_t tle_line2[70] = OBDH_PARAM_POSITION_TLE_LINE2_DEFAULT_VAL;
 
-    memcpy(&tel->data.position.tle_line1, &tle_line1[0], 70U);
-    memcpy(&tel->data.position.tle_line2, &tle_line2[0], 70U);
+    void *null_ptr;
+    null_ptr = memcpy(&tel->data.position.tle_line1, &tle_line1[0], 70U);
+    null_ptr = memcpy(&tel->data.position.tle_line2, &tle_line2[0], 70U);
 
     tel->data.position.latitude             = OBDH_PARAM_POSITION_LATITUDE_DEFAULT_VAL;
     tel->data.position.longitude            = OBDH_PARAM_POSITION_LONGITUDE_DEFAULT_VAL;
