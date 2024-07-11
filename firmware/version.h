0dbd3772f102dbdff044377b93d32325bcad54c9
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-07-11 18:23:36 -0300

    firmware: Update the firmware version

--- firmware/version.h
+++ firmware/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.10.16
+ * \version 0.10.17
  * 
  * \date 2019/10/25
  * 
@@ -36,7 +36,7 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define FIRMWARE_VERSION            "0.10.16"
+#define FIRMWARE_VERSION            "0.10.17"
 
 #define FIRMWARE_STATUS             "Development"
 
