198551c6fa7988a4bd1d1e624d2db0b7d9362167
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-09-13 21:44:55 -0300

    firmware: system: sys_log: Change sys_log timestamp to sys_time

--- firmware/system/sys_log/sys_log.c
+++ firmware/system/sys_log/sys_log.c
@@ -41,6 +41,7 @@
 #include <task.h>
 
 #include <version.h>
+#include <system/system.h>
 
 #include "sys_log.h"
 #include "sys_log_config.h"
@@ -314,7 +315,7 @@ void sys_log_print_system_time(void)
     sys_log_set_color(SYS_LOG_SYSTEM_TIME_COLOR);
 
     sys_log_print_msg("[ ");
-    sys_log_print_uint(xTaskGetTickCount());    /* System time in milliseconds */
+    sys_log_print_uint(system_get_time());    /* System time in seconds */
     sys_log_print_msg(" ]");
 
     sys_log_reset_color();
