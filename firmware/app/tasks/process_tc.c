7ad9ddc4ed0e5d19e1917a4bb32aac482e38c660
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2024-09-24 20:43:46 -0300

    firmware: app: tasks: process_tc: Add log messages for TC feedback

--- firmware/app/tasks/process_tc.c
+++ firmware/app/tasks/process_tc.c
@@ -2163,6 +2163,9 @@ static int8_t send_tc_feedback(uint8_t *pkt)
 
     if (sat_data_buf.obdh.data.mode != OBDH_MODE_HIBERNATION)
     {
+        sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_PROCESS_TC_NAME, "Transmitting \"TC Feedback\"...");
+        sys_log_new_line();
+
         if (ttc_send(TTC_0, feedback_pkt, feedback_pkt_len) != 0)
         {
             sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_PROCESS_TC_NAME, "Error transmitting a \"TC Feedback\"!");
