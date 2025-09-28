28a226ffa05c5f840a4089900417e5ac43dd4f74
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-09-28 02:13:15 -0300

    firmware: app: tasks: data_log: Remove unnused modules from data log

--- firmware/app/tasks/data_log.c
+++ firmware/app/tasks/data_log.c
@@ -137,54 +137,6 @@ void vTaskDataLog(void *p)
 
         (void)memset(&page_buf[0], 0, 256);
 
-        /* Antenna data */
-        (void)memcpy(&page_buf[0], (void*)&sat_data_buf.antenna, sizeof(antenna_telemetry_t));
-        if (mem_mng_write_data_to_flash_page(page_buf, &sat_data_buf.obdh.data.media.last_page_ant_data, nor_info.page_size, CONFIG_MEM_ANT_DATA_START_PAGE, CONFIG_MEM_ANT_DATA_END_PAGE) == 0)
-        {
-            sys_log_print_event_from_module(SYS_LOG_INFO, TASK_DATA_LOG_NAME, "Writing to Antenna sector, flash page number: ");
-            sys_log_print_hex(sat_data_buf.obdh.data.media.last_page_ant_data);
-            sys_log_new_line();
-        }
-        else
-        {
-            sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_DATA_LOG_NAME, "Error writing the antenna data to the flash memory!");
-            sys_log_new_line();
-        }
-
-        (void)memset(&page_buf[0], 0, 256);
-
-        /* EDC data */
-        (void)memcpy(&page_buf[0], (void*)sat_data_buf.state.c_edc, sizeof(edc_telemetry_t));
-        if (mem_mng_write_data_to_flash_page(page_buf, &sat_data_buf.obdh.data.media.last_page_edc_data, nor_info.page_size, CONFIG_MEM_EDC_DATA_START_PAGE, CONFIG_MEM_EDC_DATA_END_PAGE) == 0)
-        {
-            sys_log_print_event_from_module(SYS_LOG_INFO, TASK_DATA_LOG_NAME, "Writing to EDC sector, flash page number: ");
-            sys_log_print_hex(sat_data_buf.obdh.data.media.last_page_edc_data);
-            sys_log_new_line();
-        }
-        else
-        {
-            sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_DATA_LOG_NAME, "Error writing the EDC data to the flash memory!");
-            sys_log_new_line();
-        }
-
-        (void)memset(&page_buf[0], 0, 256);
-
-        /* Payload-X data */
-        (void)memcpy(&page_buf[0], (void*)&sat_data_buf.payload_x, sizeof(payload_telemetry_t));
-        if (mem_mng_write_data_to_flash_page(page_buf, &sat_data_buf.obdh.data.media.last_page_px_data, nor_info.page_size, CONFIG_MEM_PX_DATA_START_PAGE, CONFIG_MEM_PX_DATA_END_PAGE) == 0)
-        {
-            sys_log_print_event_from_module(SYS_LOG_INFO, TASK_DATA_LOG_NAME, "Writing to Payload X sector, flash page number: ");
-            sys_log_print_hex(sat_data_buf.obdh.data.media.last_page_px_data);
-            sys_log_new_line();
-        }
-        else
-        {
-            sys_log_print_event_from_module(SYS_LOG_ERROR, TASK_DATA_LOG_NAME, "Error writing the Payload-X data to the flash memory!");
-            sys_log_new_line();
-        }
-
-        (void)memset(&page_buf[0], 0, 256);
-
         vTaskDelayUntil(&last_cycle, pdMS_TO_TICKS(TASK_DATA_LOG_PERIOD_MS));
     }
 }
