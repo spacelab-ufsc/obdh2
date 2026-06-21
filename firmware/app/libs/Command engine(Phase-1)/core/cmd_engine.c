3fd48f4fb3a5abd0b5b8d4b45d96588d6dee1a36
Aditya Jindal <24uec091@lnmiit.ac.in>
2026-06-21 15:08:41 +0530

    Refactor command engine handlers to accept binary telecommand buffers

--- firmware/app/libs/Command engine(Phase-1)/core/cmd_engine.c
+++ firmware/app/libs/Command engine(Phase-1)/core/cmd_engine.c
@@ -1,44 +1,67 @@
-#include <stdio.h>
-#include <string.h>
+/*
+ * cmd_engine.c
+ * 
+ * Copyright The OBDH 2.0 Contributors.
+ * 
+ * This file is part of OBDH 2.0.
+ * 
+ * OBDH 2.0 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OBDH 2.0 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OBDH 2.0. If not, see <http://gnu.org>.
+ * 
+ */
+
+/**
+ * \brief Command engine routing execution and matching look up loops.
+ * 
+ * \author Your Name <your.email@example.com>
+ * 
+ * \version 1.0.0
+ * 
+ * \date 2026/06/21
+ * 
+ * \addtogroup cmd_engine
+ * \{
+ */
+
+#include <stddef.h>
 #include "cmd_engine.h"
 
-static command *g_table;
-static int g_count;
+static const cmd_entry_t *g_cmd_table = NULL;
+static uint8_t g_cmd_count = 0;
 
-void CMD_Init(command *table,int count){
-    g_table = table;
-    g_count = count;
-
+void cmd_engine_init(const cmd_entry_t *table, uint8_t count)
+{
+    g_cmd_table = table;
+    g_cmd_count = count;
 }
 
-static int ParseLine(char *line,
-                     char *argv[])
+void cmd_engine_execute(uint8_t cmd_id, uint8_t *pkt, uint16_t pkt_len, bool is_scheduled)
 {
-    int argc = 0;
-
-    char *token =
-        strtok(line, " ");
-
-    while(token != NULL)
+    /* Safety-critical boundary guards preventing null exceptions */
+    if ((g_cmd_table == NULL) || (pkt == NULL) || (g_cmd_count == 0))
     {
-        argv[argc++] = token;
-
-        token =
-            strtok(NULL, " ");
+        return;
     }
 
-    return argc;
-}
-
-void executeCmd(char *cmd){
-    
-    char *argv[10];
-    int argc = ParseLine(cmd,argv);
-
-     for(int i=0;i<g_count;i++){
-        if(strcmp(argv[0],g_table[i].name)==0){
-            g_table[i].handler(argc,argv);
+    /* Safe deterministic integer verification avoiding unsafe string operations */
+    for (uint8_t i = 0; i < g_cmd_count; i++)
+    {
+        if (g_cmd_table[i].id == cmd_id)
+        {
+            g_cmd_table[i].handler(pkt, pkt_len, is_scheduled);
             return;
         }
-     }
+    }
 }
+
+/** \} End of addtogroup cmd_engine */
